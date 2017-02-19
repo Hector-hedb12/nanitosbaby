201569ae371e81a900845d19e3834966f62f2d69
Hector Dominguez <hedb_12@hotmail.com>
2017-02-19 19:29:05 -0400

    use a `ManyToManyField` to manage product sizes

--- nanitosbaby/store/models.py
+++ nanitosbaby/store/models.py
@@ -20,10 +20,19 @@ class Category(models.Model):
 class Size(models.Model):
     SIZE_CHOICES = Choices('XS', 'S', 'M', 'L', 'XL', 'XXL')
     name = models.CharField(_('Nombre'), max_length=5, choices=SIZE_CHOICES)
-    amount = models.IntegerField(_('Cantidad'), default=0)
 
     def __str__(self):
         return self.name
+
+
+@python_2_unicode_compatible
+class ProductAmount(models.Model):
+    product = models.ForeignKey('Product', models.CASCADE, verbose_name=_('Producto'))
+    size = models.ForeignKey('Size', models.CASCADE, verbose_name=_('Talla'))
+    amount = models.IntegerField(_('Cantidad'), default=0)
+
+    def __str__(self):
+        return '{} - {}'.format(self.product, self.size)
 
 
 @python_2_unicode_compatible
@@ -31,9 +40,10 @@ class Product(models.Model):
     name = models.CharField(_('Nombre'), max_length=150)
     description = models.TextField(_(u'Descripción'), default='')
     price = models.DecimalField(_('Precio'), max_digits=12, decimal_places=2, default=0)
-    amount = models.IntegerField(_('Cantidad'), default=0)
     image = models.ImageField(upload_to='products/%Y/%m/%d')
+
     category = models.ForeignKey('Category', models.SET_NULL, verbose_name=_(u'Categoría'), null=True)
+    sizes = models.ManyToManyField('Size', through='ProductAmount', verbose_name=_('Tallas'))
 
     def __str__(self):
         return self.name
