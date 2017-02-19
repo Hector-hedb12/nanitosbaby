2a67e25083a9d7abbd644b4eebb33e3496f6bd02
Hector Dominguez <hedb_12@hotmail.com>
2017-02-19 18:18:03 -0400

    initial `Category` model

--- nanitosbaby/store/models.py
+++ nanitosbaby/store/models.py
@@ -1,5 +1,14 @@
-from __future__ import unicode_literals
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+@python_2_unicode_compatible
+class Category(models.Model):
+    name = models.CharField(_(u'Categoría'), max_length=150)
+
+    def __str__(self):
+        return self.name
