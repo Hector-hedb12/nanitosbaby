f5237e040ae64bbefa4c4bc8360ba3d0b6ef9e67
Hector Dominguez <hedb_12@hotmail.com>
2017-02-19 18:48:29 -0400

    initial `Product` model.

--- nanitosbaby/store/models.py
+++ nanitosbaby/store/models.py
@@ -12,3 +12,16 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+@python_2_unicode_compatible
+class Product(models.Model):
+    name = models.CharField(_('Nombre'), max_length=150)
+    description = models.TextField(_(u'Descripción'), default='')
+    price = models.DecimalField(_('Precio'), max_digits=12, decimal_places=2, default=0)
+    amount = models.IntegerField(_('Cantidad'), default=0)
+    image = models.ImageField(upload_to='products/%Y/%m/%d')
+    category = models.ForeignKey('Category', models.SET_NULL, verbose_name=_(u'Categoría'), null=True)
+
+    def __str__(self):
+        return self.name
