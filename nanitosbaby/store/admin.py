e1c211a5398e7a3dbc7116fe9d04b32d6030a5a4
Hector Dominguez <hedb_12@hotmail.com>
2017-02-19 20:01:38 -0400

    initial admin models for store models.

--- nanitosbaby/store/admin.py
+++ nanitosbaby/store/admin.py
@@ -1,3 +1,34 @@
-from django.contrib import admin
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
 
-# Register your models here.
+from django.contrib import admin
+from .models import Category, Size, ProductAmount, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ['name']
+    list_display = ['name']
+    list_display_links = None
+    list_editable = ['name']
+
+
+@admin.register(Size)
+class SizeAdmin(admin.ModelAdmin):
+    fields = ['name']
+    list_display = ['name']
+    list_display_links = None
+    list_editable = ['name']
+
+
+@admin.register(ProductAmount)
+class ProductAmountAdmin(admin.ModelAdmin):
+    fields = ['product', 'size', 'amount']
+    list_display = ['product', 'size', 'amount']
+    list_editable = ['amount']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    fields = ['name', 'description', 'price', 'image', 'category']
+    list_display = ['name', 'description', 'price', 'category']
